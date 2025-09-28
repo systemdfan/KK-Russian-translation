ba99623e2d99617cabd107a21d6e466aa9c6d2bc
systemdfan <systemdfan@bruh.net>
2025-09-28 22:48:29 +0300

    Find-Duplicates.ps1 script added

---
+++ tools/Find-Duplicates.ps1
@@ -0,0 +1,20 @@
+﻿param(
+    [Parameter(Mandatory=$true)]
+    [string]$startFolder
+)
+$hashes = @{}
+
+Get-ChildItem -Path $startFolder -Recurse -File | ForEach-Object {
+    $hash = Get-FileHash -Path $_.FullName -Algorithm MD5
+    if ($hashes.ContainsKey($hash.Hash)) {
+        $hashes[$hash.Hash] += $_.FullName
+    } else {
+        $hashes[$hash.Hash] = @($_.FullName)
+    }
+}
+
+$hashes.GetEnumerator() | Where-Object {$_.Value.Count -gt 1} | ForEach-Object {
+    Write-Host "Found duplicates by hash $($_.Name):"
+    $_.Value | ForEach-Object { Write-Host "- $_" }
+    Write-Host ""
+}
